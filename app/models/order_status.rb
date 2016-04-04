182231d8ec9b92c5f6af37e46dc7dc30f3b36d9a
Ketan Patel <ketpat@gmail.com>
2016-04-04 23:01:32 +0100

    Implement business logic for Status model

--- app/models/order_status.rb
+++
@@ -1,2 +0,0 @@
-class OrderStatus < ActiveRecord::Base
-end
--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe Status, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Status, type: :model do
+
+  it "should have a name" do
+    status = Status.new
+    expect(status).to_not be_valid
+
+    status.name = "In Progress"
+    expect(status).to be_valid
+  end
+
+  it "should have a unique name" do
+    status1 = Status.create(name: "In Progress")
+    status2 = Status.new(name: "In Progress")
+    expect(status2).to_not be_valid
+
+    status2.name = "Dispatched"
+    expect(status2).to be_valid
+  end
+
+  describe "fetching orders" do
+
+    before do
+      create_helper_objects
+    end
+
+    it "should get a list of orders for a status" do
+      @order.status = @in_progress_status
+      @order.save
+      expect(@in_progress_status.orders.count).to eq 1
+    end
+
+  end
+
 end
