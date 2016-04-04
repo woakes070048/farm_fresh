755af8cc024bdd08e789b256be47a138db8236e3
Ketan Patel <ketpat@gmail.com>
2016-04-04 13:01:06 +0100

    Added category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404105133) do
+ActiveRecord::Schema.define(version: 20160404114214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image"
+    t.integer  "parent_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "favourite_farms", force: :cascade do |t|
     t.integer  "restaurant_id"
---
+++ spec/models/category_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe Category, type: :model do
+
+  before do
+    Category.destroy_all
+    @meat_category = Category.create(name: "Meat")
+  end
+
+  it "should have a name" do
+    invalid_category = Category.create
+    expect(invalid_category).to_not be_valid
+    expect(Category.count).to eq 0
+
+    valid_category = Category.create(name: "Dairy")
+    expect(valid_category).to be_valid
+    expect(Category.count).to eq 1
+  end
+
+  it "should be able to set a parent" do
+    lamb_category = Category.create(name: "Lamb", parent: @meat_category)
+    expect(lamb_category).to be_valid
+  end
+
+  it "should be able to get all it's children" do
+    expect{@meat_category.children}.to_not raise_error
+  end
+
+
+end
+
